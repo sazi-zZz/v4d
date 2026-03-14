753f8c24a11def7386c725bd1f327ca8115a106c
lesgo-Beyond <sazedurrahman707@gmail.com>
2026-03-15 03:50:48 +0600

    sad

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/includes/db.php';
-require_once __DIR__ . '/includes/functions.php';
+require_once __DIR__ . '../includes/db.php';
+require_once __DIR__ . '../includes/functions.php';
 session_start();
 require_admin();
 
